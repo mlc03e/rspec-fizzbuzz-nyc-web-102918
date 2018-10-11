02a9f43e3085414d4f6b44992893472fd8c72a25
Meghan Campbell <mlc03e@gmail.com>
2018-10-11 19:57:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 15 == 0 
+    return "FizzBuzz"
+    
+  elsif int % 3 == 0
     return "Fizz"
     
   elsif int % 5 == 0
     return "Buzz"
     
-  elsif int % 15 == 0 
-    return "FizzBuzz"
-      end
+  end
 end
 
   # Don't forget! This file needs to be 'required' in its spec file
