e5b199b94830dd87e70e4075eb874380b8818bc2
Meghan Campbell <mlc03e@gmail.com>
2018-10-11 19:25:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-# Don't forget! This file needs to be 'required' in its spec file
+def fizzbuzz(x)
+  if x / 3 
+    return "Fizz"
+    
+  if x / 5
+    return "Buzz"
+    
+  if x / 15
+    return "FizzBuzz"
+      
+  end
+end
+  # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
